dc90dc434a216815a4f71ead4bef27f1101d5ff9
CryptoThibault <thibault.chalaoux@gmail.com>
2021-04-27 14:29:08 +0200

    end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import WeatherApp from "./components/WeatherApp"
+import CityForm from "./components/CityForm"
+import { useState } from "react"
 
 function App() {
+  const [city, setCity] = useState("Paris")
   return (
     <div className="container my-4">
       <h1 className="display-3 text-center mb-4">Météo Actuelle</h1>
-      <WeatherApp />
+      <WeatherApp city={city} />
+      <CityForm setCity={setCity} />
     </div>
   )
 }
